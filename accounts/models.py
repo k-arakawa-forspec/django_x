985f98b05ed87dc2309806aef8aef0c8f707463b
karakawa <34219644-karakawa@users.noreply.replit.com>
2024-05-24 14:34:28 +0000

    Create User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+  login_id = models.CharField(max_length=20, unique=True)
+
+  nickname = models.CharField(max_length=20)
+
+  REQUIRED_FIELDS = ['login_id', 'nickname']
