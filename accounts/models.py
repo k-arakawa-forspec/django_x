025d77788a5f0cfcb87ce8e2c2e99d98faaf57d4
karakawa <34219644-karakawa@users.noreply.replit.com>
2024-05-27 15:20:26 +0000

    ログイン認証を username ではなく login_id を使用するように変更
    createsuperuser がエラーを吐き使用できなくなっている...

--- accounts/models.py
+++ accounts/models.py
@@ -3,8 +3,12 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+  # AbstractUser から継承される username を削除
+  username = None
+
   login_id = models.CharField(max_length=20, unique=True)
 
-  nickname = models.CharField(max_length=20)
+  nickname = models.CharField(max_length=100)
 
-  REQUIRED_FIELDS = ['login_id', 'nickname']
+  USERNAME_FIELD = 'login_id'
+  REQUIRED_FIELDS = ['nickname']
