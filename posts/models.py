353683bb9dbb1b44ca63b46813aa11d7a17b9efb
k-arakawa-forspec <k_arakawa@forspec.co.jp>
2024-05-29 16:01:15 +0000

    Add created_at, updated_at to Post model

--- posts/models.py
+++ posts/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Post(models.Model):
   user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
   content = models.CharField('投稿内容', max_length=255)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
 
   def __str__(self):
     return self.id
