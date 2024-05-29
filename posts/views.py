ee5b731ef0655ffacc4f546068e7977b55cf42a4
k-arakawa-forspec <k_arakawa@forspec.co.jp>
2024-05-29 15:52:39 +0000

    Add /posts/create

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from . import forms
+
 
 # Create your views here.
+class CreateView(CreateView):
+  form_class = forms.PostForm
+  template_name = "posts/create.html"
+  success_url = reverse_lazy("accounts:home")
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
