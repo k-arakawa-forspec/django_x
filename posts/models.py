251d09db1b968b2233e2e081e9053bc2c15d6d92
k-arakawa-forspec <k_arakawa@forspec.co.jp>
2024-05-29 14:58:37 +0000

    Create Post Model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+  user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+  content = models.CharField('投稿内容', max_length=255)
+
+  def __str__(self):
+    return self.id
