ec0e84ba3e86b3bf0c4574034eb22b8effef14e4
kajiyama.junpei <j_kajiyama@forspec.co.jp>
2024-06-21 22:05:00 +0900

    `posts: views` を修正

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "posts"
 
 urlpatterns = [
-    path("", views.ListView.as_view(), name="list"),
-    path("create/", views.CreateView.as_view(), name="create"),
-    path("index/", views.IndexView.as_view(), name="index"),
+    path("", views.PostListView.as_view(), name="list"),
+    path("me/", views.MeView.as_view(), name="me"),
+    path("create/", views.PostCreateView.as_view(), name="create"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,20 +1,21 @@
 from django.views.generic import CreateView, ListView
-from django.views.generic.base import TemplateView
 from django.urls import reverse_lazy
 from . import forms
 from .models import Post
 
 
-class ListView(ListView):
+class PostListView(ListView):
     template_name = "posts/list.html"
     model = Post
     ordering = ["-id"]
 
+
+class MeView(PostListView):
     def get_queryset(self):
         return super().get_queryset().filter(user_id=self.request.user.id)
 
 
-class CreateView(CreateView):
+class PostCreateView(CreateView):
     form_class = forms.PostForm
     template_name = "posts/create.html"
     success_url = reverse_lazy("accounts:home")
@@ -22,13 +23,3 @@ class CreateView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
-
-
-class IndexView(TemplateView):
-    template_name = "posts/list.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        post_list = Post.objects.all()
-        context["post_list"] = post_list
-        return context
