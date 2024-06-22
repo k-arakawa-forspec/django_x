3532bae6d344a3798474a3ba8026157ffd770c97
k-arakawa-forspec <k_arakawa@forspec.co.jp>
2024-06-22 13:56:53 +0000

    Create Profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+  user = models.OneToOneField('accounts.User', on_delete=models.CASCADE, primary_key=True)
+  self_introduction = models.CharField('自己紹介', max_length=1024)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.id
