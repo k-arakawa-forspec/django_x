fe4d330fd76965129862cd2a5e07438fed9d2e1b
kitayama-takamichi <t_kitayama@forspec.co.jp>
2024-06-30 15:16:17 +0000

    課題の内容を実装

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+
 from . import views
 
 app_name = "posts"
 
 urlpatterns = [
+    path("", views.ListView.as_view(), name="list"),
     path("create/", views.CreateView.as_view(), name="create"),
     path("index/", views.IndexView.as_view(), name="index"),
 ]
