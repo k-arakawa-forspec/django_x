096b2cf48c19ee2dde5160c8d3f46da008debf62
shinsawa <k_shinsawa@forspec.co.jp>
2024-07-12 07:52:48 +0000

    ログインユーザーの投稿をID後順にしたものを、別ルーティングで表示

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import CreateView
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 from django.urls import reverse_lazy
 from . import forms
 from .models import Post
@@ -24,3 +25,10 @@ class IndexView(TemplateView):
     post_list = Post.objects.all()
     context["post_list"] = post_list
     return context
+
+class ListView(ListView):
+  template_name = "posts/list.html"
+
+  def get_queryset(self):
+    posts = Post.objects.filter(user=self.request.user).order_by('-id')
+    return posts
