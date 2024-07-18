746a7e0a5af6fbfdbd14e8273e15c7d8c820cd38
kajiyama.junpei <j_kajiyama@forspec.co.jp>
2024-07-18 20:11:30 +0900

    課題: 1.任意のユーザのプロフィール照会画面実装

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Profile
+
 
 # Create your views here.
+class ProfileView(DetailView):
+    model = Profile
+    template_name = 'profiles/profile.html'
+
+    def get_queryset(self):
+        return super().get_queryset().select_related('user')
