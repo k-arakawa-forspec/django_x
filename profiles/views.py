d91ad00fb7e6ed3b02d469a669f31f26bb895e32
kajiyama.junpei <j_kajiyama@forspec.co.jp>
2024-08-22 18:36:32 +0900

    課題: 2.ログインユーザーのプロフィール照会画面実装
    `URL: /profiles/my/` へアクセスしたケースのみ編集ボタンを出力する制御を追加

--- profiles/views.py
+++ profiles/views.py
@@ -6,12 +6,20 @@ from .models import Profile
 class ProfileView(DetailView):
     model = Profile
     template_name = 'profiles/profile.html'
+    is_my_profile = False
 
     def get_queryset(self):
         return super().get_queryset().select_related('user')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_my_profile'] = self.is_my_profile
+        return context
+
 
 class MyProfileView(ProfileView):
+    is_my_profile = True
+
     def get_queryset(self):
         self.kwargs.update(pk=self.request.user.pk)
         return super().get_queryset()
