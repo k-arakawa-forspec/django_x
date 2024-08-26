e538f48bcb20e116235bea57153dc48b1967e437
kajiyama.junpei <j_kajiyama@forspec.co.jp>
2024-08-26 19:48:54 +0900

    PRの修正

--- profiles/views.py
+++ profiles/views.py
@@ -8,24 +8,14 @@ from django.urls import reverse_lazy
 class ProfileView(DetailView):
     model = Profile
     template_name = 'profiles/profile.html'
-
     # `OneToOneField: User.id <=> Profile.user_id` であるため、明示的な `Profile => User` の取得は不要っぽい？
-    # def get_queryset(self):
-    #     return super().get_queryset().select_related('user')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['is_my_profile'] = self.kwargs.get('pk') == self.request.user.pk
-        return context
+    # queryset = Profile.objects.select_related('user')
 
 
 class MyProfileView(ProfileView):
 
-    def get_queryset(self):
-        # Error: "Generic detail view MyProfileView must be called with either an object pk or a slug in the URLconf."
-        # PK指定がないURL設定でエラーが発生するため、ログインユーザーのPKを設定することで解消。
-        self.kwargs.update(pk=self.request.user.pk)
-        return super().get_queryset()
+    def get_object(self, queryset=None):
+        return Profile.objects.get(user=self.request.user)
 
 
 class ProfileUpdateView(UpdateView):
