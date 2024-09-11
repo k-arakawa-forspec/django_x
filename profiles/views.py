141e6d86e80fbde7392bc4fcc26b090c467d4833
Haru-RS13 <c_sudou@forspec.co.jp>
2024-09-11 02:07:34 +0000

    Updateの修正分

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from . import forms
 from django.views.generic import UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView
+from django.shortcuts import redirect, resolve_url
 
 
 # モデルのインポート
@@ -15,17 +16,26 @@ class OtherProfView(DetailView):
   model = Profile
   template_name = "profiles/prof.html"
 
-  Profile.objects.select_related('user')
+  queryset = Profile.objects.select_related('user')
 
-class MyProfView(DetailView):
+class MyProfView(LoginRequiredMixin, DetailView):
   # 自分の投稿を表示
   model = Profile
   template_name = "profiles/myprof.html"
 
-  def get_context(self):
+  def get_context(self, queryset=None):
     return Profile.objects.select_related(user = self.request.user)
 
 class ProfileUpdateView(LoginRequiredMixin, UpdateView):
   model = Profile
   template_name = "profiles/update.html"
   form_class = forms.ProfileUpdateForm
+
+  def get_success_url(self):
+    return resolve_url('profiles:myprof', pk=self.kwargs['pk'])
+
+  # contextデータ作成
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context["process_name"] = "Update"
+    return context
