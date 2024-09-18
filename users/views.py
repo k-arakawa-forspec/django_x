0d3a8b3d8b6b0cd5aedff9d9d3cfba8ce6aaa68a
k-arakawa-forspec <k_arakawa@forspec.co.jp>
2024-09-18 05:35:58 +0000

    Add URL: /users/[login_id]

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from accounts.models import User
 
 # Create your views here.
+class PostsView(DetailView):
+  model = User
+  template_name = "users/posts.html"
+  slug_url_kwarg = "login_id"
+  slug_field = "login_id"
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    # URL中の login_id に紐づくUserインスタンス
+    # (Detailviewの実装によって既に取得されている)
+    user = self.object
+    # Userに紐づくPostのリスト
+    post_list = user.post_set.all().order_by('-id')
+    context['post_list'] = post_list
+    return context
