41a6b6d50589fe43a8d1aad03b2b707eaec85661
k-arakawa-forspec <k_arakawa@forspec.co.jp>
2024-10-02 02:09:25 +0000

    Exec: python manage.py makemigrations follows

--- follows/models.py
+++ follows/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Follow(models.Model):
+  class Meta:
+    db_table = 'follows'
+
+  # User モデルからは follow_set で参照することになる
+  user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+  # related_name を指定しない場合 User モデルからはこちらも follow_set で参照することになってしまい
+  # follow_set で名前解決できなくなることになってしまうため
+  # そもそも related_name を指定しないと makemigrations でエラーとなる
+  follow_user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='follower')
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return str(self.pk)
