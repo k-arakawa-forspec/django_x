569609e5396d74ae62848fc7cf4eef2aba26f75d
k_arakawa <k_arakawa@forspec.co.jp>
2024-10-10 10:33:15 +0000

    URL: /users/[login_id] にフォローボタン追加

--- follows/views.py
+++ follows/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.views.generic import RedirectView
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+from accounts.models import User
 
 # Create your views here.
+class AddView(RedirectView):
+    pattern_name = "users:detail"
+
+    def get_redirect_url(self, *args, **kwargs):
+        to_user = get_object_or_404(User, pk=kwargs["user_id"]);
+        from_user = self.request.user
+        from_user.follow_user_set.add(to_user)
+
+        return super().get_redirect_url(login_id=to_user.login_id)
+
+class RemoveView(RedirectView):
+    pattern_name = "users:detail"
+
+    def get_redirect_url(self, *args, **kwargs):
+        to_user = get_object_or_404(User, pk=kwargs["user_id"]);
+        from_user = self.request.user
+        from_user.follow_user_set.remove(to_user)
+
+        return super().get_redirect_url(login_id=to_user.login_id)
