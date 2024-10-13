98351f924c6bef1302b8c0aa76c79132b847837f
kajiyama.junpei <j_kajiyama@forspec.co.jp>
2024-10-14 06:16:47 +0900

    画面出力データを取得

--- users/views.py
+++ users/views.py
@@ -1,11 +1,21 @@
 from django.views.generic import DetailView
 from accounts.models import User
+from posts.models import Post
 
 
 # Create your views here.
 class UserView(DetailView):
     template_name = 'users/user.html'
+    user = None
 
+    def set_user(self):
+        self.user = User.objects.get(login_id=self.kwargs['login_id'])
 
     def get_object(self):
-        return User.objects.get(login_id=self.kwargs['login_id'])
+        self.set_user()
+        return self.user
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(user=self.user).order_by('-id').select_related()
+        return context
