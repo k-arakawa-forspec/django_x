5e97d99fba15efded78a741e4c36be297ede734a
kajiyama.junpei <j_kajiyama@forspec.co.jp>
2024-10-13 15:14:31 +0900

    `URL: /users/{login_id}` を追加

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from accounts.models import User
+
 
 # Create your views here.
+class UserView(DetailView):
+    template_name = 'users/user.html'
+
+
+    def get_object(self):
+        return User.objects.get(login_id=self.kwargs['login_id'])
