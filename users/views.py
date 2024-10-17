3fae68ea54caae2413988c2813be20e635b5bda4
kitayama-takamichi <t_kitayama@forspec.co.jp>
2024-10-17 00:39:32 +0000

    レビュー指摘箇所の修正

--- users/views.py
+++ users/views.py
@@ -1,21 +1,15 @@
 from django.views.generic import DetailView
-
 from accounts.models import User
-from posts.models import Post
-from profiles.models import Profile
-
 
 class DashBoardView(DetailView):
   template_name = "users/dashboard.html"
 
   def get_object(self):
     user = User.objects.get(login_id=self.kwargs['login_id'])
-    if user != self.request.user:
-      raise Exception("403エラー")
     return user
 
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
-    context['post_list'] = Post.objects.filter(user=self.request.user).order_by('-created_at')
-    context['profile'] = Profile.objects.get(user=self.request.user)
+    user = self.object
+    context['post_list'] = user.post_set.all().order_by('-id')
     return context
