043077163e6377d6b2c0d777b876b23825fd5a0f
k_arakawa <k_arakawa@forspec.co.jp>
2024-11-06 10:15:25 +0000

    ユーザのフォロー/フォロワー情報出力を実装

--- users/views.py
+++ users/views.py
@@ -23,4 +23,49 @@ class DetailView(DetailView):
     followed = self.request.user.follow_user_set.filter(id=user.id).exists()
     context['followed'] = followed
 
+    context['follows_count'] = user.follow_user_set.count()
+    context['followers_count'] = user.follower_user_set.count()
+
+    return context
+
+class FollowsView(DetailView):
+  model = User
+  template_name = "users/follows.html"
+  slug_url_kwarg = "login_id"
+  slug_field = "login_id"
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+
+    # URL中の login_id に紐づくUserインスタンス
+    # (Detailviewの実装によって既に取得されている)
+    user = self.object
+
+    # TODO 登録降順(follows.id desc)でソートしたいところだが
+    # ManyToManyField で through を使用しておらず中間Modelがないことが影響してか
+    # 対処のしようがない？
+    follow_user_set = user.follow_user_set.select_related("profile").all()
+    context['follow_user_set'] = follow_user_set
+
+    return context
+
+class FollowersView(DetailView):
+  model = User
+  template_name = "users/followers.html"
+  slug_url_kwarg = "login_id"
+  slug_field = "login_id"
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+
+    # URL中の login_id に紐づくUserインスタンス
+    # (Detailviewの実装によって既に取得されている)
+    user = self.object
+
+    # TODO 登録降順(follows.id desc)でソートしたいところだが
+    # ManyToManyField で through を使用しておらず中間Modelがないことが影響してか
+    # 対処のしようがない？
+    follower_user_set = user.follower_user_set.select_related("profile").all()
+    context['follower_user_set'] = follower_user_set
+
     return context
