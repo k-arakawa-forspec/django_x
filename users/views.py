782f5e6651a0a55efcfd7d5617348d464640fa60
kajiyama.junpei <j_kajiyama@forspec.co.jp>
2024-11-26 23:35:51 +0900

    PR: 件数の出力フォーマットを変更
    https://github.com/k-arakawa-forspec/django_x/pull/39#discussion_r1857550502

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from decimal import Decimal, ROUND_DOWN
 from django.views.generic.detail import DetailView
 from accounts.models import User
 
@@ -27,12 +28,41 @@ class DetailView(BaseUserView):
     context['followed'] = followed
 
     # フォロー数
-    context['follow_count'] = user.follow_user_set.all().count()
+    context['follow_count'] = self.format_count(user.follow_user_set.all().count())
 
     # フォロワー数
-    context['follower_count'] = user.follower_user_set.all().count()
+    context['follower_count'] = self.format_count(user.follower_user_set.all().count())
 
     return context
+
+
+  """ Formatted sample.
+  n = 9 => "9"
+  n = 99 => "99"
+  n = 999 => "0.9K"
+  n = 1,000 => "1.0K"
+  n = 9,999 => "9.9K"
+  n = 99,999 => "99.9K"
+  n = 999,999 => "999.9K"
+  n = 1,000,000 => "1.0M"
+  n = 9,999,999 => "9.9M"
+  n = 99,999,999 => "99.9M"
+  n = 999,999,999 => "999.9M"
+  n = 1,000,000,000 => "1.0B"
+  n = 9,999,999,999 => "9.9B"
+  """
+  def format_count(self, n):
+    units = ['K', 'M', 'B']
+    u = ''
+    for unit in units:
+      tmp = n / 1000
+      if tmp < 0.1: break
+      n = tmp
+      u = unit
+      if n / 1000 < 1: break
+
+    n = str(Decimal(str(n)).quantize(Decimal('.1'), rounding=ROUND_DOWN)) if u != '' else str(n)
+    return n + u
 
 
 class FollowsView(BaseUserView):
