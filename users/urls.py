c8336f991bd7d63528d36f8b6e1422e719ab27fe
t_kitayama <t_kitayama@forspec.co.jp>
2024-12-12 14:55:42 +0000

    12月分課題

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,6 @@ app_name = "users"
 
 urlpatterns = [
     path("<slug:login_id>/", views.DetailView.as_view(), name="detail"),
+    path("<slug:login_id>/follows/", views.FollowListView.as_view(), name="follow_list"),
+    path("<slug:login_id>/followers/", views.FollowerListView.as_view(), name="follower_list")
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,9 @@
 from django.views.generic.detail import DetailView
+from django.views.generic import ListView
 from accounts.models import User
+from accounts.models import Profile
+import logging
+logger = logging.getLogger("django")
 
 # Create your views here.
 class DetailView(DetailView):
@@ -23,4 +27,37 @@ class DetailView(DetailView):
     followed = self.request.user.follow_user_set.filter(id=user.id).exists()
     context['followed'] = followed
 
+    #フォロー・フォロワー数を取得
+    context['follow_count'] = user.follow_user_set.count()
+    context['follower_count'] = user.follower_user_set.count()
+    return context
+
+class FollowListView(ListView):
+  model = User
+  template_name = "users/follow_list.html"
+  slug_url_kwarg = "login_id"
+  slug_field = "login_id"
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    login_id = self.kwargs.get('login_id')
+    user = User.objects.get(login_id=login_id)
+
+    context['profile_user'] = user
+    context['follow_list'] = user.follow_user_set.select_related('profile')
+    return context
+
+class FollowerListView(ListView):
+  model = User
+  template_name = "users/follower_list.html"
+  slug_url_kwarg = "login_id"
+  slug_field = "login_id"
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    login_id = self.kwargs.get('login_id')
+    user = User.objects.get(login_id=login_id)
+    
+    context['profile_user'] = user
+    context['follower_list'] = user.follower_user_set.select_related('profile')
     return context
