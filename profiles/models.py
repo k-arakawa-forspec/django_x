8cb7351248f602ec529ad2d7d15c3dd666f57637
a_fujimaru <a_fujimaru@forspec.co.jp>
2025-01-13 09:57:37 +0000

    12月課題

--- profiles/models.py
+++ profiles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 def user_profile_image_path(instance, filename):
-  return filename
+  return f'users/{instance.user.pk}/profile.{filename.split(".")[-1]}'
 
 # Create your models here.
 class Profile(models.Model):
@@ -10,7 +10,7 @@ class Profile(models.Model):
 
   user = models.OneToOneField('accounts.User', on_delete=models.CASCADE, primary_key=True)
   self_introduction = models.CharField('自己紹介', max_length=1024)
-  image = models.ImageField(upload_to=user_profile_image_path, blank=True, null=True)
+  image = models.ImageField('プロフィール画像', upload_to=user_profile_image_path, blank=True, null=True)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
 
