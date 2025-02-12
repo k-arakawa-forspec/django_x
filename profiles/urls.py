2bc1bfd6dbfe78f368a5dbefd3ed6d1a8d5fd655
k_ikeda <k_ikeda@forspec.co.jp>
2025-02-12 18:05:01 +0000

    m/202501

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("my/", views.DetailLoginUserView.as_view(), name="detail_login_user"),
     path("my/update/", views.UpdateLoginUserView.as_view(), name="update_login_user"),
+    path("my/color-mode/", ColorModeUpdateView.as_view(), name="color_mode_update"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,33 +1,42 @@
-from django.urls.base import reverse
-from django.views.generic import DetailView, UpdateView
+from django.urls import reverse
+from django.views.generic import DetailView, UpdateView, View
+from django.http import HttpResponseRedirect
 from . import forms
 from .models import Profile
 
-# Create your views here.
 class DetailView(DetailView):
-  template_name = "profiles/detail.html"
-  model = Profile
+    template_name = "profiles/detail.html"
+    model = Profile
 
 class DetailLoginUserView(DetailView):
-  template_name = "profiles/detail.html"
+    template_name = "profiles/detail.html"
 
-  """
-  URL上に `pk` パラメータを設けられないため
-  get_object() をオーバーライドして直接取得
-  """
-  def get_object(self):
-    return Profile.objects.get(user=self.request.user)
+    def get_object(self):
+        return Profile.objects.get(user=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["color_mode"] = self.request.COOKIES.get("color_mode", "light")  # デフォルトはライトモード
+        return context
 
 class UpdateLoginUserView(UpdateView):
-  form_class = forms.ProfileForm
-  template_name = "profiles/update_login_user.html"
+    form_class = forms.ProfileForm
+    template_name = "profiles/update_login_user.html"
 
-  """
-  URL上に `pk` パラメータを設けられないため
-  get_object() をオーバーライドして直接取得
-  """
-  def get_object(self):
-    return Profile.objects.get(user=self.request.user)
+    def get_object(self):
+        return Profile.objects.get(user=self.request.user)
 
-  def get_success_url(self):
-    return reverse('profiles:detail_login_user')
+    def get_success_url(self):
+        return reverse('profiles:detail_login_user')
+
+class ColorModeUpdateView(View):
+    """
+    カラーモードを変更するビュー
+    """
+    def post(self, request, *args, **kwargs):
+        current_mode = request.COOKIES.get("color_mode", "light")
+        new_mode = "dark" if current_mode == "light" else "light"
+
+        response = HttpResponseRedirect(request.META.get("HTTP_REFERER", reverse("profiles:detail_login_user")))
+        response.set_cookie("color_mode", new_mode, max_age=365*24*60*60)  # 1年間保持
+        return response
