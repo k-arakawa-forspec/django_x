90b2dfb578c5fac6af80020512508339c3cbedcf
k_ikeda <k_ikeda@forspec.co.jp>
2025-03-12 17:46:41 +0000

    m/202502

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    nickname = models.CharField(max_length=30, unique=True)
+
+    def get_following_users(self):
+        """ フォローしているユーザーの一覧を取得 """
+        return User.objects.filter(followers__follower=self)
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]  # 投稿を降順に
--- home/views.py
+++ home/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView
+from posts.models import Post
+from django.contrib.auth import get_user_model
 
-# Create your views here.
-class IndexView(TemplateView):
-  template_name = "home/index.html"
+User = get_user_model()
+
+class ndexView(TemplateView):
+    template_name = "home/index.html"
+    context_object_name = "posts"
+    paginate_by = 10  # 一旦1ページに10件表示
+
+    def get_queryset(self):
+        """ フォローしているユーザーの投稿を取得（投稿降順） """
+        user = self.request.user
+        if user.is_authenticated:
+            return Post.objects.filter(user__in=user.get_following_users()).select_related("user").order_by("-created_at")
+        return Post.objects.none()  # 未ログインなら空
