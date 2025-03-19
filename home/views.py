fe916026965c9aed65efdacaebceb23f3a0aa0a0
a_fujimaru <a_fujimaru@forspec.co.jp>
2025-03-19 06:53:45 +0000

    指摘修正

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView
 from posts.models import Post
 
 # Create your views here.
@@ -7,21 +7,11 @@ class IndexView(TemplateView):
   template_name = "home/index.html"
 
   def get_context_data(self, **kwargs):
+    # フォローしているユーザーIDを一度に取得
+    followed_user_ids = self.request.user.follow_user_set.values_list('id', flat=True)
+    # フォローしているユーザーのポストと関連情報を降順で取得
+    post_list = Post.objects.select_related('user__profile').filter(user__id__in=followed_user_ids).order_by('-created_at')
+
     context = super().get_context_data(**kwargs)
-    post_list = Post.objects.all().order_by('-created_at')
     context["post_list"] = post_list
     return context
-
-class ListView(ListView):
-  template_name = "posts/list.html"
-  model = Post
-
-  def get_queryset(self):
-    user = self.request.user
-    # ログインユーザーがフォローしているユーザーのIDを取得
-    followed_users = user.follow_user_set.all()
-
-    # フォローしているユーザーのポストを降順で取得
-    queryset = Post.objects.filter(user__in=followed_users).order_by('-created_at')
-
-    return queryset
