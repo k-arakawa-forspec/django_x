fa12a1226bb26fa2196de92cb0b8f778847cc53f
kajiyama.junpei <j_kajiyama@forspec.co.jp>
2025-03-23 20:47:13 +0900

    ListViewでの実装へ修正

--- home/views.py
+++ home/views.py
@@ -1,17 +1,15 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 from posts.models import Post
 
 
 # Create your views here.
-class IndexView(TemplateView):
+class IndexView(ListView):
   template_name = "home/index.html"
+  model = Post
+  ordering = ['-created_at']
 
-  def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs)
-
-    # ログインユーザーがフォローしているユーザーIDのリストを取得する。
+  def get_queryset(self):
+    queryset = super().get_queryset()
     ids = [followed_users.id for followed_users in self.request.user.follow_user_set.all()]
-    post_list = Post.objects.filter(user__in=ids).order_by('-created_at')
-    context['post_list'] = post_list
-
-    return context
+    queryset = queryset.filter(user__in=ids)
+    return queryset
